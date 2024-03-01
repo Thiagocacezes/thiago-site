98f0b05f0de779a22c41013b4d646b67df89df58
Thiago cacezes <124006612+Thiagocacezes@users.noreply.github.com>
2024-03-01 20:37:17 +0000

    cookies1

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-if (!localStorage.getItem("cookiesAccepted")) { 
-    var cookieMessage = document.getElementById('cookie-notification');  
+if (!localStorage.getItem("cookiesAccepted")) {
+    var cookieMessage = document.getElementById('cookie-notification');
     var closeCookie = document.getElementById('cookie-notification-close');
-    
-    cookieMessage.style.display = 'block';  
-    closeCookie.addEventListener("click", function(e) {  
-      e.preventDefault();
-      localStorage.setItem("cookiesAccepted", true);
-      
-      cookieMessage.style.display = 'none';
+
+    cookieMessage.style.display = 'block';
+    closeCookie.addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.setItem("cookiesAccepted", true);
+
+        cookieMessage.style.display = 'none';
     });
 }
