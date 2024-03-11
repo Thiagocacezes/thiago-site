6ab9937576b3043b857368d956304879e536f59f
Thiago cacezes <124006612+Thiagocacezes@users.noreply.github.com>
2024-03-11 13:40:50 +0000

    j

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-if (!localStorage.getItem("cookiesAccepted")) {
+document.addEventListener("DOMContentLoaded", function () {
+    var cookieMessage = document.getElementById('cookie-notification');
+    var closeCookie = document.getElementById('cookie-notification-close');
+
+    // Exibir o aviso de cookies
+    cookieMessage.style.display = 'block';
+
+    closeCookie.addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.setItem("cookiesAccepted", true);
+
+        // Ocultar o aviso de cookies após aceitar
+        cookieMessage.style.display = 'none';
+    });
+
+    // Verificar se os cookies já foram aceitos anteriormente
+    if (localStorage.getItem("cookiesAccepted")) {
+        // Se os cookies foram aceitos anteriormente, ocultar o aviso
+        cookieMessage.style.display = 'none';
+    }
+});
+/*if (!localStorage.getItem("cookiesAccepted")) {
     var cookieMessage = document.getElementById('cookie-notification');
     var closeCookie = document.getElementById('cookie-notification-close');
 
@@ -9,7 +30,7 @@ if (!localStorage.getItem("cookiesAccepted")) {
 
         cookieMessage.style.display = 'none';
     });
-}
+}*/
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
