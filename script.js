7b15715a508e37a7bf89219d9e1a00ca0c6c45c9
Thiago cacezes <124006612+Thiagocacezes@users.noreply.github.com>
2024-03-11 14:02:05 +0000

    ok

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var cookieMessage = document.getElementById('cookie-notification');
-
-    // Exibir o aviso de cookies
-    cookieMessage.style.display = 'block';
-});
-/*document.addEventListener("DOMContentLoaded", function () {
-    var cookieMessage = document.getElementById('cookie-notification');
-    var closeCookie = document.getElementById('cookie-notification-close');
-
-    // Exibir o aviso de cookies
-    cookieMessage.style.display = 'block';
-
-    closeCookie.addEventListener("click", function (e) {
-        e.preventDefault();
-        localStorage.setItem("cookiesAccepted", true);
-
-        // Ocultar o aviso de cookies após aceitar
-        cookieMessage.style.display = 'none';
-    });
-
-    // Verificar se os cookies já foram aceitos anteriormente
-    if (localStorage.getItem("cookiesAccepted")) {
-        // Se os cookies foram aceitos anteriormente, ocultar o aviso
-        cookieMessage.style.display = 'none';
-    }
-});*/
-/*if (!localStorage.getItem("cookiesAccepted")) {
+if (!localStorage.getItem("cookiesAccepted")) {
     var cookieMessage = document.getElementById('cookie-notification');
     var closeCookie = document.getElementById('cookie-notification-close');
 
@@ -36,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         cookieMessage.style.display = 'none';
     });
-}*/
+}
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
