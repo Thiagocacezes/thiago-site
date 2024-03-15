1885dc2218b3c993b1d4f3fea20a7540dd5a27b2
Thiago cacezes <124006612+Thiagocacezes@users.noreply.github.com>
2024-03-15 18:09:37 +0000

    ...oo...

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
+const cookieNotification = document.getElementById('aviso-de-cookies');
+const closeButton = document.getElementById('aviso-de-cookies-fechar');
+
+function closeCookieNotification() {
+    cookieNotification.style.display = 'none';
+}
+
+closeButton.addEventListener('click', closeCookieNotification);
+
+const cookieConsent = getCookie('cookieConsent');
+
+if (!cookieConsent) {
+    cookieNotification.style.display = 'block';
+}
+
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()}`;
+}
+
+function getCookie(name) {
+    const cookieValue = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
+    return cookieValue ? cookieValue[2] : null;
+}
+
+/*document.addEventListener("DOMContentLoaded", function() {
     if (!localStorage.getItem("cookiesAccepted")) {
         var cookieMessage = document.getElementById('cookie-notification');
         var closeCookie = document.getElementById('cookie-notification-close');
@@ -12,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
     }
-});
+});*/
 /*if (!localStorage.getItem("cookiesAccepted")) {
     var cookieMessage = document.getElementById('cookie-notification');
     var closeCookie = document.getElementById('cookie-notification-close');
